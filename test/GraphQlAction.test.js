9817a51c068c52e5ba4adf37f2b2502de2e90cb3
Alex J Burke <alex@alexjeffburke.com>
2019-11-24 13:17:06 +0100

    Rearrange GraphQlAction test file.

--- test/GraphQlAction.test.js
+++ test/GraphQlAction.test.js
@@ -10,60 +10,38 @@ describe("GraphQlAction", () => {
     expect(GraphQlAction, "to be a function");
   });
 
-  it("should have a static method for creating a query", () => {
-    expect(GraphQlAction.query, "to be a function");
-  });
-
-  it("should have a static method for creating a mutation", () => {
-    expect(GraphQlAction.mutation, "to be a function");
-  });
-
   it("should create a query as an instance of GraphQlAction", () => {
-    const action = GraphQlAction.query();
+    const action = new GraphQlAction({ type: GRAPHQL_QUERY });
     expect(action, "to be a", GraphQlAction);
   });
 
   it("should return false from isMutation", () => {
-    const action = GraphQlAction.query();
+    const action = new GraphQlAction({ type: GRAPHQL_QUERY });
     expect(action.isMutation(), "to be false");
   });
 
   it("should return true from isQuery", () => {
-    const action = GraphQlAction.query();
+    const action = new GraphQlAction({ type: GRAPHQL_QUERY });
     expect(action.isQuery(), "to be true");
   });
 
   it("should create a mutation as an instance of GraphQlAction", () => {
-    const action = GraphQlAction.mutation();
+    const action = new GraphQlAction({ type: GRAPHQL_MUTATION });
     expect(action, "to be a", GraphQlAction);
   });
 
   it("should return true from isMutation", () => {
-    const action = GraphQlAction.mutation();
+    const action = new GraphQlAction({ type: GRAPHQL_MUTATION });
     expect(action.isMutation(), "to be true");
   });
 
   it("should return false from isQuery", () => {
-    const action = GraphQlAction.mutation();
+    const action = new GraphQlAction({ type: GRAPHQL_MUTATION });
     expect(action.isQuery(), "to be false");
   });
 
-  it("should create a query", () => {
-    const action = GraphQlAction.query();
-    expect(action, "to satisfy", {
-      type: GRAPHQL_QUERY
-    });
-  });
-
-  it("should create a mutation", () => {
-    const action = GraphQlAction.mutation();
-    expect(action, "to satisfy", {
-      type: GRAPHQL_MUTATION
-    });
-  });
-
   it("should be able to resolve a returned promise", () => {
-    const action = GraphQlAction.query();
+    const action = new GraphQlAction({ type: GRAPHQL_QUERY });
     const promise = action.getPromise();
 
     action.resolve("foo");
@@ -72,11 +50,47 @@ describe("GraphQlAction", () => {
   });
 
   it("should be able to reject a returned promise", () => {
-    const action = GraphQlAction.query();
+    const action = new GraphQlAction({ type: GRAPHQL_QUERY });
     const promise = action.getPromise();
 
     action.reject("foo");
 
     return expect(promise, "when rejected", "to equal", "foo");
+  });
+
+  describe("query()", () => {
+    it("should have a static method for creating a query", () => {
+      expect(GraphQlAction.query, "to be a function");
+    });
+
+    it("should create a query as an instance of GraphQlAction", () => {
+      const action = GraphQlAction.query();
+      expect(action, "to be a", GraphQlAction);
+    });
+
+    it("should create a query", () => {
+      const action = GraphQlAction.query();
+      expect(action, "to satisfy", {
+        type: GRAPHQL_QUERY
+      });
+    });
+  });
+
+  describe("mutation()", () => {
+    it("should have a static method for creating a mutation", () => {
+      expect(GraphQlAction.mutation, "to be a function");
+    });
+
+    it("should create a mutation as an instance of GraphQlAction", () => {
+      const action = GraphQlAction.mutation();
+      expect(action, "to be a", GraphQlAction);
+    });
+
+    it("should create a mutation", () => {
+      const action = GraphQlAction.mutation();
+      expect(action, "to satisfy", {
+        type: GRAPHQL_MUTATION
+      });
+    });
   });
 });
